cd7616ed19495862918838e59a9311e18af8a79c
DURepo <doshna.u@gmail.com>
2019-11-19 16:12:46 -0800

    removed note in results

--- client/src/containers/Studies/StudyResult.js
+++ client/src/containers/Studies/StudyResult.js
@@ -18,7 +18,7 @@ class StudyResult extends React.Component{
                 <div>
                     <h4>In the study that you did:</h4>
                     <h5>Based on the data you reported there is a <u>statistically significant impact </u> of "{this.state.study.observed_input}" on "{this.state.study.observed_output}".</h5>
-                    <p style={{fontSize:"15px"}}>Note, there might be other factors as well that are influencing the results.</p>
+                    
                 </div>
             )
         }
@@ -27,7 +27,7 @@ class StudyResult extends React.Component{
                 <div>
                          <h4>In the study that you did:</h4>
                          <h5>Based on the data you reported there is a <u>no statistically significant impact</u> of "{this.state.study.observed_input}"" on "{this.state.study.observed_output}".</h5>
-                         <p style={{fontSize:"15px"}} >Note, there might be other factors as well that are influencing the results.</p>
+                        
                 </div>)
         } 
     }
